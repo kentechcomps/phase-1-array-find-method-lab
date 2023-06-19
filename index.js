1601f162523dde6c94add2d0347391d5553b9055
user <mutukukennedy34@gmail.com>
2023-06-19 12:52:36 +0300

    findmethod

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+const record = [
+    {
+        year : '2023',
+        result : 'Win'
+    }
+    ,
+    {
+        year : '2022',
+        result : 'N/A'
+    }
+]
+
+const recordwin = record.find(ourfun);
+
+console.log(recordwin);
+
+
+function ourfun(value) {
+    return value.result === 'Win'
+}
